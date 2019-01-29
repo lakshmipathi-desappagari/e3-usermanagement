c3906aaa4ffd6f0455014bfc372c2023dbdb12e5
Karthik k r <kr.karthik11@gmail.com>
2019-01-29 23:35:53 +0000

    Sample SpringBoot Project
    This is just a sample project to render a page

---
+++ e3/src/main/java/com/ethree/EthreeApplication.java
@@ -0,0 +1,14 @@
+package com.ethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthreeApplication.class, args);
+	}
+
+}
+
